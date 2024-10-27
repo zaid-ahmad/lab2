bb99ed28e9af391ae7d624c7bcc9cd5ae5fc4038
Zaid Ahmad <zaidd250@gmail.com>
2024-10-27 14:41:56 -0600

    complete lab3

--- app/index.js
+++ app/index.js
@@ -4,15 +4,16 @@
  * @format
  */
 
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView, StyleSheet } from "react-native";
 import ToDoList from "../components/ToDoList";
 import ToDoForm from "../components/ToDoForm";
 
 function App() {
+    const [tasks, setTasks] = useState(["Do laundry", "Go to gym", "Walk dog"]);
     return (
         <SafeAreaView style={styles.container}>
-            <ToDoList />
+            <ToDoList tasks={tasks} />
             <ToDoForm />
         </SafeAreaView>
     );
--- components/ToDoList.jsx
+++ components/ToDoList.jsx
@@ -1,24 +1,16 @@
 import React from "react";
 import { ScrollView, Pressable, View, Text, StyleSheet } from "react-native";
 
-const ToDoList = () => {
+const ToDoList = ({ tasks }) => {
     return (
         <ScrollView>
-            <Pressable>
-                <View style={[styles.task, styles.completed]}>
-                    <Text style={styles.taskText}>Do laundry</Text>
-                </View>
-            </Pressable>
-            <Pressable>
-                <View style={[styles.task]}>
-                    <Text style={styles.taskText}>Go to gym</Text>
-                </View>
-            </Pressable>
-            <Pressable>
-                <View style={[styles.task, styles.completed]}>
-                    <Text style={styles.taskText}>Walk dog</Text>
-                </View>
-            </Pressable>
+            {tasks.map((task, index) => (
+                <Pressable key={index}>
+                    <View style={styles.task}>
+                        <Text style={styles.taskText}>{task}</Text>
+                    </View>
+                </Pressable>
+            ))}
         </ScrollView>
     );
 };
